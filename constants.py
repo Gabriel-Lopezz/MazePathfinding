20be0a63d23723a08ad8c5d54a4dc36fc30417fa
duckly216 <jorgep020905@gmail.com>
2025-07-24 17:49:56 -0400

    added new colors

--- constants.py
+++ constants.py
@@ -1,13 +1,21 @@
+#==SIZES==#
+# pasted from sudoku project, needs updating
 WINDOW_SIZE = 750
 WIDTH = WINDOW_SIZE #PIXEL WIDTH
 LENGTH = WINDOW_SIZE #PIXEL HEIGHT
 NUM_FONT = WINDOW_SIZE // 10
-BG_COLOR = (255, 255, 245)
-#==Colors==#
-BLACK = (200, 200, 200)
-RED = (196, 29, 0)
-SKETCHED_COLOR = (116, 115, 122) #grey
-BOLD = (5, 5, 5)
-SMALL_BOX_COLOR = 125, 125, 125
 BLOCK_SIZE = WINDOW_SIZE / 9
+#==COLORS==# if you change a color, comment what color it is
+BLACK = (0, 0, 0)
+RED = (196, 29, 0)
+START_COLOR = (46,111,64) #forest green
+GOAL_COLOR = (255, 255, 0) #yellow
+PATH_COLOR = (211,211,211) #light grey
+EXPLORED_COLOR = (116, 115, 122) #darker grey
+FINAL_PATH_COLOR = START_COLOR
+WALL_COLOR = BLACK
+INTERSECTION_COLOR = (0,255,255) #aqua
+
+#==OTHERS==#
+BOLD = (5, 5, 5)
 OPTIONS_FONT = 65

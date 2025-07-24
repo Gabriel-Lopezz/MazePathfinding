332809963d1e7670e05a923e425c621be12542a5
duckly216 <jorgep020905@gmail.com>
2025-07-24 17:51:57 -0400

    properly creates maze 2D array rep.

--- maze.py
+++ maze.py
@@ -1,5 +1,18 @@
-class maze:
-    def __init__(self, columns, rows):
-        "creates a maze with all open spaces"
-        "maybe it can take in a string and then just initialises it with that info"
-        self.maze = [['.' for _ in range(columns)] for _ in range(rows)]
+import pygame, sys
+class Maze:
+    def __init__(self, input_maze, width, height, screen):
+        """
+        :param input_maze: a string representation of the maze 
+        :param width:
+        :param height:
+        :param screen:
+        screen is a window from PyGame.
+        creates a maze from a string input space, which should be separated by \n
+        """
+        self.maze = [list(row) for row in input_maze.strip().split('\n')]
+        self.width = width
+        self.height = height
+        self.screen = screen
+    def print_maze(self):
+        for row in self.maze:
+            print(row)

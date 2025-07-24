d6f9f50271fa844e6832dbdbc2e4493442d62ca9
duckly216 <jorgep020905@gmail.com>
2025-07-24 17:45:01 -0400

    Fixed constructor to properly set text

--- button.py
+++ button.py
@@ -10,7 +10,7 @@ class Button:
         
         self.text = text
         self.font = Font(None, 36)
-        self.text_surface = self.font.render("TEST", True, (0,0,0), None)
+        self.text_surface = self.font.render(text, True, (0,0,0), None)
         self.text_rect = self.text_surface.get_rect(center=(self.rect.centerx, self.rect.centery))
     
     def draw(self):

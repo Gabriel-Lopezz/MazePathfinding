243d6258013fe0fa8efc9f8971153b9185b1368b
duckly216 <jorgep020905@gmail.com>
2025-07-29 17:12:50 -0400

    Added identifiers to constructor variables, and added dictionary to get coordinates from the vertex number

--- Algorithms.py
+++ Algorithms.py
@@ -4,23 +4,11 @@ from collections import deque
 from maze import Maze
 from block import BlockState
 
-def get_adjacent(x,y,col,rows, maze_array):
-    dx = [1, -1, 0, 0] # right, left
-    dy = [0, 0, 1, -1] # down, up
-    adjacents = set()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx < 0 or ny < 0 or nx >= col or ny >= rows or maze_array[ny][nx].state == BlockState.WALL:
-          continue
-        adjacents.add((nx,ny))
-    return adjacents
 
 def BFS_for_intersections(start_point : tuple , maze_obj : Maze):
     '''
     Looks for all the intersection points of the maze to be added to the adjacency list
     '''
-    count = 0
     start_x, start_y = start_point
     columns = maze_obj.cols, rows = maze_obj.rows
     array_rep = maze_obj.maze_array
@@ -32,20 +20,20 @@ def BFS_for_intersections(start_point : tuple , maze_obj : Maze):
     # stored as (x, y)
     q = deque()
     q.append((start_x,start_y))
-    intersections = set()
+
     while q.count !=0:
         ux, uy = q.popleft()
         neighbors = deque() # empty queue
-        for vx,vy in get_adjacent(ux,uy,columns,rows,array_rep):
+        for vx,vy in array_rep.get_adjacent(ux,uy):
           if not visited[vy][vx]:
             visited[vy][vx] = True
 
             q.append((vx,vy))
             neighbors.append((vx,vy))
         if len(neighbors) >= 3: #intersection if it has >2 adjacent open paths
-            intersections.add((ux,uy))
+            print("This is an intersection")
 
 
-    return intersections
+    return 1
 
 

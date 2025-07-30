ab3f479d865bd8b84b0ada26873e16f6cdaa2bfa
duckly216 <jorgep020905@gmail.com>
2025-07-30 16:44:09 -0400

    Finishing up traversal algorithm to collect all graph points, considering changing to stack approach

--- Algorithms.py
+++ Algorithms.py
@@ -12,6 +12,7 @@ def BFS_for_intersections(start_point : tuple , maze_obj : Maze):
     start_x, start_y = start_point
     columns = maze_obj.cols, rows = maze_obj.rows
     array_rep = maze_obj.maze_array
+    maze_graph = maze_obj.graph_points
 
     # Keeps track of Visited
     visited = [[False for i in range(columns)] for j in range(rows)]
@@ -22,18 +23,17 @@ def BFS_for_intersections(start_point : tuple , maze_obj : Maze):
     q.append((start_x,start_y))
 
     while q.count !=0:
+        # ux, uy is the "center" point
         ux, uy = q.popleft()
-        neighbors = deque() # empty queue
-        for vx,vy in array_rep.get_adjacent(ux,uy):
+        adjacents = array_rep.get_adjacent(ux,uy)
+        for vx,vy in adjacents:
           if not visited[vy][vx]:
             visited[vy][vx] = True
-
             q.append((vx,vy))
-            neighbors.append((vx,vy))
-        if len(neighbors) >= 3: #intersection if it has >2 adjacent open paths
-            print("This is an intersection")
+        if maze_obj.is_valid_graph_point((ux,uy),adjacents): #a valid graph point is either a start/end point, intersection, turn, or dead-end
+            maze_graph.add_connection()
 
 
-    return 1
+    return True
 
 

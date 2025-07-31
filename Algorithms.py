5850e7ee8a811e40253b90b749f31e80e110e94a
duckly216 <jorgep020905@gmail.com>
2025-07-30 22:27:19 -0400

    no longer doing BFS/DFS for maze graph point collection, method will be in maze class

--- Algorithms.py
+++ Algorithms.py
@@ -5,35 +5,39 @@ from maze import Maze
 from block import BlockState
 
 
-def BFS_for_intersections(start_point : tuple , maze_obj : Maze):
-    '''
-    Looks for all the intersection points of the maze to be added to the adjacency list
-    '''
-    start_x, start_y = start_point
-    columns = maze_obj.cols, rows = maze_obj.rows
-    array_rep = maze_obj.maze_array
-    maze_graph = maze_obj.graph_points
-
-    # Keeps track of Visited
-    visited = [[False for i in range(columns)] for j in range(rows)]
-    visited[start_y][start_x] = True
-
-    # stored as (x, y)
-    q = deque()
-    q.append((start_x,start_y))
-
-    while q.count !=0:
-        # ux, uy is the "center" point
-        ux, uy = q.popleft()
-        adjacents = array_rep.get_adjacent(ux,uy)
-        for vx,vy in adjacents:
-          if not visited[vy][vx]:
-            visited[vy][vx] = True
-            q.append((vx,vy))
-        if maze_obj.is_valid_graph_point((ux,uy),adjacents): #a valid graph point is either a start/end point, intersection, turn, or dead-end
-            maze_graph.add_connection()
-
-
-    return True
+# def BFS_for_intersections(start_point : tuple , maze_obj : Maze):
+#     '''
+#     Looks for all the intersection points of the maze to be added to the adjacency list
+#     '''
+#     start_x, start_y = start_point
+#     columns , rows = maze_obj.cols, maze_obj.rows
+#     array_rep = maze_obj.maze_array
+#     maze_graph = maze_obj.graph_points
+#     # keeps track of the distance between each maze coord that would go in the graph
+#     distance = [[-1 for i in range(columns)] for j in range(rows)]
+#     distance[start_y][start_x] = 0
+#     # Keeps track of Visited
+#     visited = [[False for i in range(columns)] for j in range(rows)]
+#     visited[start_y][start_x] = True
+#
+#     # stored as (x, y)
+#     q = deque()
+#     q.append((start_x,start_y))
+#     from_point = start_x,start_y
+#     while q:
+#         # ux, uy is the "center" point, or current
+#         ux, uy = q.popleft()
+#         adjacents = array_rep.get_adjacent(ux,uy)
+#         for vx,vy in adjacents:
+#           if not visited[vy][vx]:
+#             visited[vy][vx] = True
+#             distance[vy][vx] = distance[uy][ux] + 1
+#             curr_distance = distance[vy][vx]
+#             q.append((vx,vy))
+#         if maze_obj.is_valid_graph_point((ux,uy),adjacents): #a valid graph point is either a start/end point, intersection, turn, or dead-end
+#             maze_graph.add_connection(from_point,(ux,uy),curr_distance)
+#
+#
+#     return True
 
 

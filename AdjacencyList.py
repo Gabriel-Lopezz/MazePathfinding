051cc40d8913c2df52ef9c4da23df94a142b1357
duckly216 <jorgep020905@gmail.com>
2025-08-01 05:29:05 -0400

    Added more type annotations for clarity. Created print_list for debugging

--- AdjacencyList.py
+++ AdjacencyList.py
@@ -4,21 +4,32 @@ class AdjacencyList:
         """Constructor for Adjacency List Object"""
         '''
         each intersection will be labled based on a vertex number, or its ID
+        i.e: the base graph variable has a key: vertex number, and value of (connection, distance)
         the coord_to_verNum dictionary will store the vertex # as a value, with the key being the ID
         '''
+
         self.graph : dict[int, list[tuple[int,int]]] = {}
         self.vertex_num : int = 0
         # "Maps the vertex(intersection) to it's coordinate from the maze"
-        self.intersection_coords : set[tuple] = set() # stores what coords have been saved
-        self.coord_to_verNum : dict[tuple,int] = {} # key: coordinate (y,x), value: vertex #
-        self.verNum_to_coord : dict[int,tuple] = {} # key: vertex #, value: coordinate (y,x)
+        self.intersection_coords : set[tuple[int,int]] = set() # stores what coords have been saved
+        self.coord_to_verNum : dict[tuple[int,int],int] = {} # key: coordinate (y,x), value: vertex #
+        self.verNum_to_coord : dict[int,tuple[int,int]] = {} # key: vertex #, value: coordinate (y,x)
     # Getters : for debugging
     def get_vertex_number(self, coord):
         return self.coord_to_verNum.get(coord)
     def get_vertices(self):
         """returns number of vertices"""
         return len(self.graph)
-    def add_connection(self, _from:tuple, _to:tuple, _weight):
+    def print_list(self):
+        the_graph = self.graph
+        for vertexNum, pair in the_graph.items():
+            print(f"{vertexNum}: ")
+            for value in pair:
+                connection, distance = value
+                print(f"->[{connection}:{distance}]")
+
+
+    def add_connection(self, _from:tuple[int,int], _to:tuple[int,int], _weight):
         # Checks if the from and to coordinates have already been added to the list
         if _from not in self.intersection_coords:
             self.intersection_coords.add(_from)
